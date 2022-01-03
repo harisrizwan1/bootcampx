6bda4d70e85e5370d539f0be92aa91cc77a570b4
harisrizwan1 <harisrizwan985@gmail.com>
2022-01-03 17:12:56 -0600

    finish 2_queries_join

---
+++ 2_queries_joins/1_total_assignment_duration.sql
@@ -0,0 +1,4 @@
+SELECT SUM(duration)
+FROM students JOIN assignment_submissions ON students.id = student_id
+WHERE name = 'Ibrahim Schimmel'
+;
---
+++ 2_queries_joins/2_cohort_assignment_duration.sql
@@ -0,0 +1,6 @@
+SELECT SUM(duration)
+FROM students 
+JOIN assignment_submissions ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12'
+;
