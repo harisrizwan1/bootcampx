762924e31022c16486b8d95b94b81cefe697ddaa
harisrizwan1 <harisrizwan985@gmail.com>
2022-01-05 19:58:45 -0600

    sanitize inputs

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,10 +13,10 @@ FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teachers.name
 ;
-`)
+`, [`%${process.argv[2] || 'JUL02'}%`])
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
